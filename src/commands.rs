4c6f37845eabc6aacf22657f8a76531a92cd193a
Sam Gammon <s@mg.sv>
2024-11-15 17:14:53 -0800

    chore: fmt
    Signed-off-by: Sam Gammon <s@mg.sv>

--- src/commands.rs
+++ src/commands.rs
@@ -1,17 +1,14 @@
 use std::sync::Mutex;
-use tauri::{AppHandle, command, Runtime, Manager};
+use tauri::{command, AppHandle, Manager, Runtime};
 
 use crate::models::*;
-use crate::{PushTokenState, Result};
 use crate::PushNotificationsExt;
+use crate::{PushTokenState, Result};
 
 #[command]
-pub(crate) async fn push_token<R: Runtime>(
-    app: AppHandle<R>,
-) -> Result<PushTokenResponse> {
+pub(crate) async fn push_token<R: Runtime>(app: AppHandle<R>) -> Result<PushTokenResponse> {
     let state = app.state::<Mutex<PushTokenState>>();
 
-    app
-        .push_notifications()
+    app.push_notifications()
         .get_push_token(state, PushTokenRequest {})
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use tauri::{plugin::{Builder, TauriPlugin}, Manager, RunEvent, Runtime};
+use tauri::{
+    plugin::{Builder, TauriPlugin},
+    Manager, RunEvent, Runtime,
+};
 
 pub use models::*;
 use std::{collections::HashMap, sync::Mutex};
@@ -31,50 +34,49 @@ struct PushTokenStore(Mutex<HashMap<String, String>>);
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the push-notifications APIs.
 pub trait PushNotificationsExt<R: Runtime> {
-  fn push_notifications(&self) -> &PushNotifications<R>;
+    fn push_notifications(&self) -> &PushNotifications<R>;
 }
 
 impl<R: Runtime, T: Manager<R>> PushNotificationsExt<R> for T {
-  fn push_notifications(&self) -> &PushNotifications<R> {
-    self.state::<PushNotifications<R>>().inner()
-  }
+    fn push_notifications(&self) -> &PushNotifications<R> {
+        self.state::<PushNotifications<R>>().inner()
+    }
 }
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-  Builder::new("push-notifications")
-    .invoke_handler(tauri::generate_handler![commands::push_token])
-    .setup(|app, api| {
-      // setup push token storage
-      app.manage(PushTokenStore(Default::default()));
-      app.manage(Mutex::new(PushTokenState::default()));
-      #[cfg(mobile)]
-      let push_notifications = mobile::init(app, api)?;
-      #[cfg(desktop)]
-      let push_notifications = desktop::init(app, api)?;
-      app.manage(push_notifications);
-      Ok(())
-    })
-    .on_event(|app, event| {
-      match event {
-        RunEvent::PushRegistration(token) => {
-            let state = app.state::<Mutex<PushTokenState>>();
-            let mut state = state.lock().unwrap();
-            let owned = token.to_owned();
-            state.token = Some(owned);
-        }
+    Builder::new("push-notifications")
+        .invoke_handler(tauri::generate_handler![commands::push_token])
+        .setup(|app, api| {
+            // setup push token storage
+            app.manage(PushTokenStore(Default::default()));
+            app.manage(Mutex::new(PushTokenState::default()));
+            #[cfg(mobile)]
+            let push_notifications = mobile::init(app, api)?;
+            #[cfg(desktop)]
+            let push_notifications = desktop::init(app, api)?;
+            app.manage(push_notifications);
+            Ok(())
+        })
+        .on_event(|app, event| {
+            match event {
+                RunEvent::PushRegistration(token) => {
+                    let state = app.state::<Mutex<PushTokenState>>();
+                    let mut state = state.lock().unwrap();
+                    let owned = token.to_owned();
+                    state.token = Some(owned);
+                }
 
-        RunEvent::PushRegistrationFailed(err) => {
-            let state = app.state::<Mutex<PushTokenState>>();
-            let mut state = state.lock().unwrap();
-            let owned = err.to_owned();
-            state.err = Some(owned);
-        }
+                RunEvent::PushRegistrationFailed(err) => {
+                    let state = app.state::<Mutex<PushTokenState>>();
+                    let mut state = state.lock().unwrap();
+                    let owned = err.to_owned();
+                    state.err = Some(owned);
+                }
 
-        // @TODO: token persistence?
-
-        _ => {}
-      }
-    })
-    .build()
+                // @TODO: token persistence?
+                _ => {}
+            }
+        })
+        .build()
 }
