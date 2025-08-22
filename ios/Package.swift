9437d47e9f8fb17de22d5ba425c0cddd3a871e17
lucas <sauronlucas@gmail.com>
2025-08-22 15:14:30 +0200

    added firebase dependencies

--- ios/Package.swift
+++ ios/Package.swift
@@ -21,7 +21,7 @@ let package = Package(
   dependencies: [
     .package(name: "Tauri", path: "../.tauri/tauri-api")
     .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
-    .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")<
+    .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
