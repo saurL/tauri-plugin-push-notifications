0acb1fd542b2172d491c607fe138e9fec04517f1
lucas <sauronlucas@gmail.com>
2025-08-22 16:24:59 +0200

    added firebase dependencies

--- ios/Package.swift
+++ ios/Package.swift
@@ -32,8 +32,8 @@ let package = Package(
       name: "tauri-plugin-push-notifications",
       dependencies: [
         .byName(name: "Tauri"),
-        .byName(name: "FirebaseCore"),
-        .byName(name: "FirebaseMessaging")
+        .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
+        .product(name: "FirebaseMessaging", package: "firebase-ios-sdk")
       ],
       path: "Sources")
   ]
