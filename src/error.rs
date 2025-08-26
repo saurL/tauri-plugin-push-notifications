ce6b3c3e105be9c0d0ad4f6e5ce15b3bfbdd120f
lucas <sauronlucas@gmail.com>
2025-08-26 11:03:10 +0200

    a

--- src/error.rs
+++ src/error.rs
@@ -19,3 +19,24 @@ impl Serialize for Error {
         serializer.serialize_str(self.to_string().as_ref())
     }
 }
+#[cfg(mobile)]
+impl From<&str> for PluginInvokeError {
+    fn from(s: &str) -> Self {
+        PluginInvokeError::InvokeRejected(crate::error::ErrorResponse {
+            code: None,
+            message: Some(s.to_string()),
+            data: (),
+        })
+    }
+}
+
+#[cfg(mobile)]
+impl From<String> for PluginInvokeError {
+    fn from(s: String) -> Self {
+        PluginInvokeError::InvokeRejected(crate::error::ErrorResponse {
+            code: None,
+            message: Some(s),
+            data: (),
+        })
+    }
+}
